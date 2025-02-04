3c504a8d1fc4b7bc92bb9a8344454180f41eab73
iBala <p.balakumaran@gmail.com>
2025-02-04 08:42:03 +0530

    changed metadata

--- app/metadata.ts
+++ app/metadata.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
-  name: "AppliedAI",
-  description: "Micro-agents to 100x your team's productivity. Automate your menial tasks with AI. AppliedAI's agents make your life easier.",
+  name: "AppliedAI - Your AI army",
+  description: "A suite of micro-agents that take care of all your menial tasks and 100x your team's productivity.",
   url: "https://appliedai.club",
   ogImage: "https://appliedai.club/og.png", // You'll need to create this image
   links: {
